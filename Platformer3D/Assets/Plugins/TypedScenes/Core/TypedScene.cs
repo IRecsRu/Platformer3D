ddce1421f3297f8adf7b55806b6ea30ba40ba1b8
IRecsRu <89744179+IRecsRu@users.noreply.github.com>
2022-05-20 15:37:36 +0300

    Add Plugins

---
+++ Platformer3D/Assets/Plugins/TypedScenes/Core/TypedScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace IJunior.TypedScenes
+{
+    public abstract class TypedScene
+    {
+        protected static async Task<SceneInstance> LoadScene(string sceneName, LoadSceneMode loadSceneMode)
+        {
+            await Addressables.DownloadDependenciesAsync(sceneName).Task;
+            SceneInstance sceneInstance = await Addressables.LoadSceneAsync(sceneName, loadSceneMode).Task;
+            return sceneInstance;
+        }
+    }
+}
---
+++ Platformer3D/Assets/Plugins/TypedScenes/Core/TypedSceneGenerator.cs
@@ -0,0 +1,70 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace IJunior.TypedScenes
+{
+    public static class TypedSceneGenerator
+    {
+        public static string Generate(AnalyzableScene scene)
+        {
+            var sceneName = scene.Name;
+            var targetUnit = new CodeCompileUnit();
+            var targetNamespace = new CodeNamespace(TypedSceneSettings.Namespace);
+            var targetClass = new CodeTypeDeclaration(sceneName);
+            targetNamespace.Imports.Add(new CodeNamespaceImport("System.Threading.Tasks"));
+            targetNamespace.Imports.Add(new CodeNamespaceImport("UnityEngine.SceneManagement"));
+            targetNamespace.Imports.Add(new CodeNamespaceImport("UnityEngine.ResourceManagement.ResourceProviders"));
+            targetClass.BaseTypes.Add("TypedScene");
+            targetClass.TypeAttributes = System.Reflection.TypeAttributes.Class | System.Reflection.TypeAttributes.Public;
+
+            AddConstantValue(targetClass, typeof(string), "_sceneName", sceneName);
+
+            AddLoadingMethod(targetClass);
+
+            targetNamespace.Types.Add(targetClass);
+            targetUnit.Namespaces.Add(targetNamespace);
+
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+
+            var code = new StringWriter();
+            provider.GenerateCodeFromCompileUnit(targetUnit, code, options);
+
+            return code.ToString();
+        }
+
+        private static void AddConstantValue(CodeTypeDeclaration targetClass, Type type, string name, string value)
+        {
+            var pathConstant = new CodeMemberField(type, name);
+            pathConstant.Attributes = MemberAttributes.Private | MemberAttributes.Const;
+            pathConstant.InitExpression = new CodePrimitiveExpression(value);
+            targetClass.Members.Add(pathConstant);
+        }
+
+        private static void AddLoadingMethod(CodeTypeDeclaration targetClass)
+        {
+            var loadMethod = new CodeMemberMethod();
+            loadMethod.Name = "LoadAsync";
+            loadMethod.Attributes = MemberAttributes.Public | MemberAttributes.Static;
+
+            var loadingStatement = "LoadScene(_sceneName, loadSceneMode)";
+            loadMethod.ReturnType = new CodeTypeReference(typeof(Task<SceneInstance>));
+            loadingStatement = "return " + loadingStatement;
+
+            var loadingModeParameter = new CodeParameterDeclarationExpression(nameof(LoadSceneMode), "loadSceneMode = LoadSceneMode.Single");
+            loadMethod.Parameters.Add(loadingModeParameter);
+
+            loadMethod.Statements.Add(new CodeSnippetExpression(loadingStatement));
+            targetClass.Members.Add(loadMethod);
+        }
+    }
+}
+#endif
